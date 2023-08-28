825c263c2553a6e67ef6d6d728135734f17cc4dd
kimyurie <9911dbfl@naver.com>
2023-08-28 23:53:42 +0900

    design : shade banner

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-
+// menu
 const $menu = document.querySelector('menu');
 const $hide_menu = document.querySelector('.hide_menu');
 const padding = 64;
@@ -15,3 +15,24 @@ $menu.addEventListener('mouseout',() => {
     $hide_menu.style.padding = 0;
 });
 
+// shade banner
+let shadeBannerIndex = 0;
+const $shadebanner = document.querySelectorAll('#shade-banner > ul > li');
+const $shadebannerPrevBtn = document.querySelector('.shade-prev-btn');
+const $shadebannerNextBtn = document.querySelector('.shade-next-btn');
+
+function prevShadeBanner() {
+    if(shadeBannerIndex <= 0){
+        shadeBannerIndex = $shadebanner.length - 1;
+    } else {
+        shadeBannerIndex--;
+    }
+
+    for (let item of $shadebanner) {
+        item.style.opacity = 0;
+    }
+
+    $shadebanner[shadeBannerIndex].style.opacity = 1;
+}
+
+$shadebannerPrevBtn.addEventListener('click', prevShadeBanner);
