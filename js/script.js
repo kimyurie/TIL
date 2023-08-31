ea3049ef4dea4d0bbac72a6a13adcc6503db1657
kimyurie <9911dbfl@naver.com>
2023-08-31 23:16:32 +0900

    feat: shade banner

--- js/script.js
+++ js/script.js
@@ -16,15 +16,15 @@ $menu.addEventListener('mouseout',() => {
 });
 
 // shade banner
-let shadeBannerIndex = 0;
-const $shadebanner = document.querySelectorAll('#shade-banner > ul > li');
+let shadeBannerIndex = 0; // 다음 버튼을 누를때마다 인덱스 번호가 1씩 증가
+const $shadebanner = document.querySelectorAll('#shade-banner > ul > li'); // ㄴ 1,2,3번 가져오기
 const $shadebannerPrevBtn = document.querySelector('.shade-prev-btn');
 const $shadebannerNextBtn = document.querySelector('.shade-next-btn');
 
 function prevShadeBanner() {
-    if(shadeBannerIndex <= 0){
-        shadeBannerIndex = $shadebanner.length - 1;
-    } else {
+    if(shadeBannerIndex <= 0){  // 1 -> 3으로 갈 경우
+        shadeBannerIndex = $shadebanner.length - 1; // ㄴ길이 - 1 = 맨뒤 인덱스로 바꿈 
+    } else {  // 3->2 , 2-> 1 으로 갈 경우
         shadeBannerIndex--;
     }
 
@@ -35,4 +35,20 @@ function prevShadeBanner() {
     $shadebanner[shadeBannerIndex].style.opacity = 1;
 }
 
+
+function nextShadeBanner() {
+    if(shadeBannerIndex >= $shadebanner.length - 1){ // 3 -> 1 
+        shadeBannerIndex = 0;    
+    } else { // 1->2, 2->3
+        shadeBannerIndex++;
+    }
+
+    for (let item of $shadebanner){
+        item.style.opacity = 0;
+    }
+
+    $shadebanner[shadeBannerIndex].style.opacity = 1;
+}
+
 $shadebannerPrevBtn.addEventListener('click', prevShadeBanner);
+$shadebannerNextBtn.addEventListener('click', nextShadeBanner)
