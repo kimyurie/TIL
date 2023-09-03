71fded90851e14fd9d3f856b7eff28a6b7c9639c
kimyurie <9911dbfl@naver.com>
2023-09-03 19:43:20 +0900

    Add : 인기순 영화 정보 데이터 가져와 state에 넣기

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,7 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { FaCode } from "react-icons/fa";
+import { API_KEY, API_URL } from '../../Config';
+// import { response } from 'express';
 
 function LandingPage() {
+
+    const [Movies, setMovies] = useState([])
+
+
+    // 영화 정보 가져오기 위해 useEffect 활용
+    useEffect(() => {
+        // 현재 인기있는 영화 정보 가져오기
+        const endpoint = `${API_URL}/movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+
+        fetch(endpoint)
+        .then(response => response.json())
+        .then(response => {
+            setMovies([response.result])
+        })
+
+    }, [])
+
 
     return (
         <div style={{width: '100%', margin: '0'}}>
