a7e1f6865a052cf6cee00a3211b7d88783c36cfa
kimyurie <9911dbfl@naver.com>
2023-09-03 18:59:29 +0900

    Design : LangingPage

---
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { FaCode } from "react-icons/fa";
+
+function LandingPage() {
+
+    return (
+        <div style={{width: '100%', margin: '0'}}>
+            {/* Main Image */}
+            <div style={{width: '85%', margin: '1rem auto'}}>
+                <h2>Movies by latest</h2>
+                <hr/>
+                {/* Movie Grid Cards */}         
+            </div>
+
+            <div style={{display:'flex', justifyContent:'center'}}>
+                <button>Load More</button>
+            </div>
+
+
+
+        </div>
+    )
+}
+
+export default LandingPage
