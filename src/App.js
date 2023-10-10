445f156914f4c96420301fdb04c8626b3aea5e17
kimyurie <9911dbfl@naver.com>
2023-10-10 23:24:55 +0900

    Feat: 리액트 라우터

--- src/App.js
+++ src/App.js
@@ -4,48 +4,64 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container, Nav, Navbar } from 'react-bootstrap';
 import data from './data';
 import { useState } from 'react';
+import { Route, Routes, Link, useNavigate, Outlet } from 'react-router-dom';
+import Detail from './routes/Detail';
 
 function App() {
  let [shoes] = useState(data);
+ let navigate = useNavigate();
 
   return (
     <div>
-    <Navbar bg="dark" variant="dark">
-      <Container>
-      <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-      <Nav className="me-auto">
-        <Nav.Link href="#home">Home</Nav.Link>
-        <Nav.Link href="#features">Features</Nav.Link>
-        <Nav.Link href="#pricing">Pricing</Nav.Link>
-      </Nav>
-      </Container>
-    </Navbar>
-    <div className='main-bg'></div>
-    <div className="container">
-        <div className="row">
-          {/* <div className="col-md-4">
-            <img src={process.env.PUBLIC_URL + '/logo192.png'}width="80%" />
-            <h4>{shoes[0].title}</h4>
-            <p>{shoes[0].price}</p>
-          </div>
-          <div className="col-md-4">
-            <img src="https://codingapple1.github.io/shop/shoes2.jpg" width="80%" />
-            <h4>{shoes[1].title}</h4>
-            <p>{shoes[1].price}</p>
-          </div>
-          <div className="col-md-4">
-            <img src="https://codingapple1.github.io/shop/shoes3.jpg" width="80%" />
-            <h4>{shoes[2].title}</h4>
-            <p>{shoes[2].price}</p>
-          </div> */}
-          <Card shoes = {shoes[0]} i = {1}/>
-          <Card shoes = {shoes[1]} i = {2}/>
-          <Card shoes = {shoes[2]} i = {3}/>
-        </div>
-      </div>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+        <Navbar.Brand href="#home">Shop</Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#features">Cart</Nav.Link>
+          <Nav.Link href="#pricing">Detail</Nav.Link>
+        </Nav>
+        </Container>
+      </Navbar>
+    
+      <Routes>
+          <Route path = "/detail/:id" element={<Detail shoes = {shoes}/>}/>
+          <Route path="/" element = {
+            <div>
+              <div className='main-bg'></div>
+                  <div className="container">
+                  <div className="row">
+                    {shoes.map((a,i) => {
+                        return <Card shoes={shoes[i]} i={i + 1} />;
+                      })
+                    }
+              </div>
+              </div>
+            </div>  
+          }/>
+          <Route path="*" element={<div>없는 페이지임</div>}/>
+          <Route path='/about' element ={<About/>}>
+              <Route path='member' element={<div>멤버들</div>}/>
+              <Route path='location' element={<div>회사위치</div>}/>
+          </Route>
+      </Routes>
+      
+      
+      {/* <button onClick={() => {navigate('/detail')}}>이동버튼</button> */}
   </div>
   )
 }
+
+function About(){
+  return (
+    <div>
+      어바웃페이지임
+      <Outlet></Outlet>
+    </div>
+   
+  )
+}
+
 
 function Card(props){
   return (
