03ed9cab9836cad8801312ecc3887901135ccee3
kimyurie <9911dbfl@naver.com>
2023-10-10 20:35:49 +0900

    Feat: Card 컴포넌트 만들기까지

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import data from './data';
+import { useState } from 'react';
 
 function App() {
+ let [shoes] = useState(data);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div>
+    <Navbar bg="dark" variant="dark">
+      <Container>
+      <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+      <Nav className="me-auto">
+        <Nav.Link href="#home">Home</Nav.Link>
+        <Nav.Link href="#features">Features</Nav.Link>
+        <Nav.Link href="#pricing">Pricing</Nav.Link>
+      </Nav>
+      </Container>
+    </Navbar>
+    <div className='main-bg'></div>
+    <div className="container">
+        <div className="row">
+          {/* <div className="col-md-4">
+            <img src={process.env.PUBLIC_URL + '/logo192.png'}width="80%" />
+            <h4>{shoes[0].title}</h4>
+            <p>{shoes[0].price}</p>
+          </div>
+          <div className="col-md-4">
+            <img src="https://codingapple1.github.io/shop/shoes2.jpg" width="80%" />
+            <h4>{shoes[1].title}</h4>
+            <p>{shoes[1].price}</p>
+          </div>
+          <div className="col-md-4">
+            <img src="https://codingapple1.github.io/shop/shoes3.jpg" width="80%" />
+            <h4>{shoes[2].title}</h4>
+            <p>{shoes[2].price}</p>
+          </div> */}
+          <Card shoes = {shoes[0]} i = {1}/>
+          <Card shoes = {shoes[1]} i = {2}/>
+          <Card shoes = {shoes[2]} i = {3}/>
+        </div>
+      </div>
+  </div>
+  )
+}
+
+function Card(props){
+  return (
+    <div className="col-md-4">
+      <img src= {"https://codingapple1.github.io/shop/shoes" + props.i + ".jpg" }width="80%" />
+      <h4>{props.shoes.title}</h4>
+      <p>{props.shoes.price}</p>
+  </div>
+  )
 }
 
 export default App;
