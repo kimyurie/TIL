51cf285d7cbec151507e04610d5aab975d8fc83e
kimyurie <9911dbfl@naver.com>
2023-10-11 13:50:49 +0900

    Feat: Lifecycle & useEffect

--- src/App.js
+++ src/App.js
@@ -1,76 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container, Nav, Navbar } from 'react-bootstrap';
-import data from './data';
-import { useState } from 'react';
-import { Route, Routes, Link, useNavigate, Outlet } from 'react-router-dom';
-import Detail from './routes/Detail';
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import data from "./data";
+import { useState } from "react";
+import { Route, Routes, Link, useNavigate, Outlet } from "react-router-dom";
+import Detail from "./routes/Detail";
 
 function App() {
- let [shoes] = useState(data);
- let navigate = useNavigate();
+  let [shoes] = useState(data);
+  let navigate = useNavigate();
 
   return (
     <div>
       <Navbar bg="dark" variant="dark">
         <Container>
-        <Navbar.Brand href="#home">Shop</Navbar.Brand>
-        <Nav className="me-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#features">Cart</Nav.Link>
-          <Nav.Link href="#pricing">Detail</Nav.Link>
-        </Nav>
+          <Navbar.Brand href="#home">Shop</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Cart</Nav.Link>
+            <Nav.Link href="#pricing">Detail</Nav.Link>
+          </Nav>
         </Container>
       </Navbar>
-    
+
       <Routes>
-          <Route path = "/detail/:id" element={<Detail shoes = {shoes}/>}/>
-          <Route path="/" element = {
+        <Route path="/detail/:id" element={<Detail shoes={shoes} />} />
+        <Route
+          path="/"
+          element={
             <div>
-              <div className='main-bg'></div>
-                  <div className="container">
-                  <div className="row">
-                    {shoes.map((a,i) => {
-                        return <Card shoes={shoes[i]} i={i + 1} />;
-                      })
-                    }
+              <div className="main-bg"></div>
+              <div className="container">
+                <div className="row">
+                  {shoes.map((a, i) => {
+                    return <Card shoes={shoes[i]} i={i + 1} />;
+                  })}
+                </div>
               </div>
-              </div>
-            </div>  
-          }/>
-          <Route path="*" element={<div>없는 페이지임</div>}/>
-          <Route path='/about' element ={<About/>}>
-              <Route path='member' element={<div>멤버들</div>}/>
-              <Route path='location' element={<div>회사위치</div>}/>
-          </Route>
+            </div>
+          }
+        />
+        <Route path="*" element={<div>없는 페이지임</div>} />
+        <Route path="/about" element={<About />}>
+          <Route path="member" element={<div>멤버들</div>} />
+          <Route path="location" element={<div>회사위치</div>} />
+        </Route>
       </Routes>
-      
-      
+
       {/* <button onClick={() => {navigate('/detail')}}>이동버튼</button> */}
-  </div>
-  )
+    </div>
+  );
 }
 
-function About(){
+function About() {
   return (
     <div>
       어바웃페이지임
       <Outlet></Outlet>
     </div>
-   
-  )
+  );
 }
 
-
-function Card(props){
+function Card(props) {
   return (
     <div className="col-md-4">
-      <img src= {"https://codingapple1.github.io/shop/shoes" + props.i + ".jpg" }width="80%" />
+      <img
+        src={"https://codingapple1.github.io/shop/shoes" + props.i + ".jpg"}
+        width="80%"
+      />
       <h4>{props.shoes.title}</h4>
       <p>{props.shoes.price}</p>
-  </div>
-  )
+    </div>
+  );
 }
 
 export default App;
--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,43 +1,50 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import styled from "styled-components";
 
-let Box = styled.div`
-  padding: 20px;
-  color: grey;
-`;
+function Detail(props) {
+  let { id } = useParams();
+  let 찾은상품 = props.shoes.find((x) => x.id == id);
+  let [box, setBox] = useState(true);
+  let [num, setNum] = useState('');
 
-let YellowBtn = styled.button`
-   background : ${props => props.bg};
-  color : ${props => props.bg == 'blue' ? 'white' : 'black'};
-  padding : 10px;
-`
+  useEffect(() => {
+    let a = setTimeout(() => {setBox(false)}, 2000)
+    return () => {
+      clearTimeout(a)
+    }
+  }, [])
 
-function Detail(props){
-    let {id} = useParams();
-    let 찾은상품 = props.shoes.find(
-        (x) => x.id == id
-    )
+  useEffect(() => {
+    if(isNaN(num) == true){
+      alert('그러지마세요')
+    }
+  }, [num])
 
-    return (
-      <div className="container">
-        <div className="col-md-6">
-          <img
-            src="https://codingapple1.github.io/shop/shoes1.jpg"
-            width="100%"
-          />
-        </div>
-        <div className="col-md-6 mt-4">
-          <h4 className="pt-5">{찾은상품.title}</h4>
-          <p>{찾은상품.content}</p>
-          <p>{찾은상품.price}원</p>
-          <button className="btn btn-danger">주문하기</button>
-        </div>
-        <Box>
-          <YellowBtn bg="orange">뇽</YellowBtn>
-          <YellowBtn bg="blue">녕</YellowBtn>
-        </Box>
+  
+
+
+  return (
+    <div className="container">
+      <div className="col-md-6">
+        <img
+          src="https://codingapple1.github.io/shop/shoes1.jpg"
+          width="100%"
+        />
       </div>
-    );
+      <div className="col-md-6 mt-4">
+        <h4 className="pt-5">{찾은상품.title}</h4>
+        <p>{찾은상품.content}</p>
+        <p>{찾은상품.price}원</p>
+        <button className="btn btn-danger">주문하기</button>
+      </div>
+      {
+        box == true ? <div className="alert alert-warning">
+        2초이내 구매시 할인
+      </div> : null
+      }
+      <input onChange={(e) => {setNum(e.target.value)}}/>
+    </div>
+  );
 }
 
 export default Detail;
